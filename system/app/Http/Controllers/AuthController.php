51fbe6d73e878136ee80d06adb332786113249ef
Muhammad Khadafi <90817924+MuhammadKhadafi07@users.noreply.github.com>
2021-11-09 14:41:21 -0800

    Update AuthController.php

--- system/app/Http/Controllers/AuthController.php
+++ system/app/Http/Controllers/AuthController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 class AuthController extends Controller
 {
     function showLogin(){
-        return view('login');
+        return view('adminlogin');
     }
 
     function processLogin(){
